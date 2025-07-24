ccc626b3be8cc41f1b74a872f4b4f89ab535e41d
Sibusiso Tshabalala <tshabalalasibuda16@gmail.com>
2025-07-24 15:12:35 +0200

    Another update

--- core/langgraph_flow.py
+++ core/langgraph_flow.py
@@ -3,9 +3,18 @@ from agents.planner_agent import planner_agent
 from agents.flight_agent import flight_agent
 from agents.hotel_agent import hotel_agent
 from agents.calender_agent import calendar_agent
+from typing import TypedDict
+
+class TripPlannerState(TypedDict):
+    destination: str
+    departure: str
+    return_date: str
+    flight_info: str
+    hotel_info: str
+    itinerary: str
 
 def build_workflow():
-    graph = StateGraph(   )
+    graph = StateGraph(TripPlannerState)
     graph.add_node("planner", planner_agent)
     graph.add_node("flight", flight_agent)
     graph.add_node("hotel", hotel_agent)
